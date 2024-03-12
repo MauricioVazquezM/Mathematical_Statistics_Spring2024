e90876a1a591bef26b4aa5c49550e304e9f29a72
Mauricio Vazquez <mvazq109@itam.mx>
2024-03-11 23:35:36 -0600

    Commit2Ass1

--- ASSIGNMENT 1/Assignment1.R
+++ ASSIGNMENT 1/Assignment1.R
@@ -68,3 +68,57 @@ out <- hist(x, xlab="observations", ylab="frecuency", main="Gamma Distribution")
 # Executing
 
 # Defining our seed and realizations number
+set.seed(12345)
+
+# Defining our variables
+K <- 51
+N <- 10000
+n <- 25
+
+# Setting our sucess probability and the complement
+p <- 0.5
+q <- 1-p
+
+# Calculating mean
+mu <- (1-p)/p
+
+# calculating standard deviation
+sigma <- (1-p)/p^2
+
+# Defining vector to keep mean of each sample
+xbar <- rep(NA,N)
+
+# Calculating the sample mean ans storing in the deined vector
+for(i in seq(N)) {
+  x <- rgeom(n, p)
+  xbar[i] <- mean(x)
+}
+
+# Creating a sequence that spans from the min to the max into K equal intervals
+x <- seq(min(xbar), max(xbar), length=K)
+
+# Defining our histogram
+tt <- hist(xbar, prob=TRUE, main="")
+
+# Defining the plot title
+lab <- substitute("Gometric Distribution: p="*p, list(p=p))
+title(lab)
+
+# Calcultaing the density of a normal distribution 
+f1 <- dnorm(x, mean=mean(xbar), sd=sd(xbar))
+
+# Adding a line to the existing histogram
+lines(x,f1, lwd=2, col=4)
+
+# calculating the observed probabilities for each bin
+p.obs <- tt$density*(tt$breaks[2]-tt$breaks[1])
+
+# Calculating the theorical probabilities for each bin, based on the normal distribution specified
+p.teo <- as.numeric(diff(pnorm(tt$breaks, mean=mean(xbar), sd=sd(xbar))))
+
+# Computing the total absoulte error between the observed and theorical probabilties
+cat("\nError_1: ",error <- sum(abs(p.obs-p.teo)))
+
+# Setting legend
+legend("topleft", legend=paste("Error = ",round(error,4)))
+
