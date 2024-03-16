6316195d1cdba369e734d4dbeb7a5669806599a3
Mauricio Vazquez <mvazq109@itam.mx>
2024-03-16 17:28:06 -0600

    Bulletb

--- ASSIGNMENT 2/Assignment2.R
+++ ASSIGNMENT 2/Assignment2.R
@@ -27,6 +27,7 @@ lambda_MoM <- function(muestra, n){
 
 # Abstracting data for each team member
 datos <- read.table("/Users/mauva/Documents/ITAM/8vo Semestre/ESTADISTICA MATEMATICA/TAREAS/TAREA2/tarea2.dat")
+n <- 30
 datos_mau <- datos[11]
 datos_aitana <- datos[13]
 datos_gianca <- datos[21]
@@ -89,34 +90,94 @@ abline(h=0)
 ################################################### End of example ####################################################
 
 # Defining functions to use
-likelihood_function <- function(samp, alpha, lambda, n){
+likelihood_function <- function(samp, alpha, lambda){
   return((((lambda**alpha)/gamma(alpha))**n)*prod(samp)**(alpha-1)*exp(-lambda*sum(samp)))
 }
 
-log_likelihoog_function <- function(samp, alpha, lambda, n){
-  log(likelihood_function(samp, alpha, lambda, n))
+log_likelihood_function <- function(samp, alpha, lambda){
+  log(likelihood_function(samp, alpha, lambda))
 }
 
 # Defining our functions f1 and f2 for Newton-Raphson iteration
-f1 <- function(a) -n*digamma(a)-n*log(mean(x))+n*log(a)+sum(log(x))
-f2 <- function(a) -n*trigamma(a)+n/a
+# This f1 function is the first partial derivative of log_likelihood function respective to alpha 
+f1 <- function(alpha,samp) -n*digamma(alpha)-n*log(mean(samp))+n*log(alpha)+sum(log(samp))
+# This f1 function is the second partial derivative of log_likelihood function respective to alpha 
+f2 <- function(alpha, samp) -n*trigamma(alpha)+n/alpha
+
+# Defining number of iterations
+iters <- 5
 
 # Defining initial guess for alpha, based on our calculations by "3.a bullet"
 x0_mau <- alpha_MME_mau
 x0_aitana <- alpha_MME_aitana
-x0-gianca <- alpha_MME_gianca
-iters <- 50
+x0_gianca <- alpha_MME_gianca
 
 # Implementing Numerical method: Newton-Raphson iterations
-for(i in 1:iters)
-  x0 <- x0 - f1(x0)/f2(x0)
+for(i in 1:iters){
+  
+  x0_mau <- x0_mau - f1(x0_mau, datos_mau$X191686)/f2(x0_mau, datos_mau$X191686)
+  
+  }
 
-# Defining alpha_hat
-alpha_hat <- x0
+for(i in 1:iters){
+  
+  x0_aitana <- x0_aitana - f1(x0_aitana, datos_aitana$X194674)/f2(x0_aitana, datos_aitana$X194674)
+  
+  }
+
+for(i in 1:iters){
+  
+  x0_gianca <- x0_gianca - f1(x0_gianca, datos_gianca$X200672)/f2(x0_gianca, datos_gianca$X200672)
+  
+  }
+
+# Defining x_bar for each team member
+x_bar_mau <- mean(datos_mau$X191686)
+x_bar_aitana <- mean(datos_aitana$X194674)
+x_bar_gianca <- mean(datos_gianca$X200672)
+
+# Defining alpha_hat for each team member
+alpha_MLE_mau <- x0_mau
+alpha_MLE_aitana <- x0_aitana
+alpha_MLE_gianca <- x0_gianca
 
 # Estimating that the maximum Likelihood Estimate of Lambda was found to be alpha_hat
-lambda_hat <- alpha_hat/x_bar
-lambda_hat
+lambda_MLE_mau <- alpha_MLE_mau/x_bar_mau
+lambda_MLE_aitana <- alpha_MLE_aitana/x_bar_aitana
+lambda_MLE_gianca <- alpha_MLE_gianca/x_bar_gianca
 
+# Displaying results of alpha and lambda
+cat("Number of iterations: ", iters)
+cat("\n")
+cat(sprintf("Maximum Likelihood Estimation alpha for Mau: %.5f\n", alpha_MLE_mau),
+    sprintf("Maximum Likelihood Estimation lambda for Mau: %.5f\n", lambda_MLE_mau))
+cat("\n")
+cat(sprintf("Maximum Likelihood Estimation alpha for Aitana: %.5f\n", alpha_MLE_aitana),
+    sprintf("Maximum Likelihood Estimation lambda for Aitana: %.5f\n", lambda_MLE_aitana))
+cat("\n")
+cat(sprintf("Maximum Likelihood Estimation alpha for Giancarlo: %.5f\n", alpha_MLE_gianca),
+    sprintf("Maximum Likelihood Estimation lambda for Giancarlo: %.5f\n", lambda_MLE_gianca))
+cat("\n")
+
+# Calculating differences between Newton-Raphson iterations
+like_mau <- likelihood_function(datos_mau$X191686, alpha_MLE_mau, lambda_MLE_mau)
+like_aitana <- likelihood_function(datos_aitana$X194674, alpha_MLE_aitana, lambda_MLE_aitana)
+like_gianca <- likelihood_function(datos_gianca$X200672, alpha_MLE_gianca, lambda_MLE_gianca)
+
+log_like_mau <- log_likelihood_function(datos_mau$X191686, alpha_MLE_mau, lambda_MLE_mau)
+log_like_aitana <- log_likelihood_function(datos_aitana$X194674, alpha_MLE_aitana, lambda_MLE_aitana)
+log_like_gianca <- log_likelihood_function(datos_gianca$X200672, alpha_MLE_gianca, lambda_MLE_gianca)
+
+# Displaying results
+cat("Number of iterations: ", iters)
+cat(sprintf("Likelihood Function Estimation for Mau: %.5e\n", like_mau),
+    sprintf("Log Likelihood Function Estimation for Mau: %.5f\n", log_like_mau))
+cat(sprintf("Likelihood Function Estimation for Aitana: %.5e\n", like_aitana),
+    sprintf("Log Likelihood Function Estimation for Aitana: %.5f\n", log_like_aitana))
+cat(sprintf("Likelihood Function Estimation for Gianca: %.5e\n", like_gianca),
+    sprintf("Log Likelihood Function Estimation for Gianca: %.5f\n", log_like_gianca))
+cat("\n")
 
 ##### BULLET "c" #####
+
+
